a6c0a75af9001cd0164f60f9a930fa9cfc561269
Amanda Farah <amandamfarah@gmail.com>
2020-12-20 15:17:30 -0600

    make dataset cached_property and debug time slices

--- jetstream/post_proc.py
+++ jetstream/post_proc.py
@@ -10,6 +10,7 @@ from scipy import fft, fftpack
 from scipy.stats import skew
 from matplotlib.colors import LogNorm
 import joypy
+os.environ['PROJ_LIB'] = '/home/afarah/.conda/envs/jetstream/share/proj/'
 import cartopy.crs as ccrs
 from cached_property import cached_property
 
@@ -25,13 +26,12 @@ class SingleModelPostProcessor(object):
         self.season = season
         self.var = diagnostic_var
 
-    def sel_winters(self,start_year=2015,end_year=2099):
+    def sel_winters(self,start_year=2015,end_year=2100):
         winters = pd.date_range('%i-12-01'%start_year,'%i-02-28'%(start_year+1),freq='D')
-        for i in range(start_year,end_year):
-            begin = i+1
-            end = i+2
+        for i in range(start_year+1,end_year):
+            begin = i
+            end = i+1
             winters=winters.union(pd.date_range('%i-12-01'%begin,'%i-02-28'%end,freq='D'))
-         
         selected = self.dataset.sel(time=winters)
         return selected
 
@@ -41,10 +41,15 @@ class SingleModelPostProcessor(object):
                                          chunks={'time': 1},
                                          combine='by_coords')
         if self.season == 'DJF':
-           if 'era5' in 'path_to_files':
-               self.dataset = self.sel_winters(1980,2018)
+           if 'era5' in self.path_to_files:
+               _dataset = self.sel_winters(1980,2018)
            else:
-               self.dataset=self.sel_winters()
+               try:
+                   self.dataset['time'] = self.dataset.indexes['time'].normalize()
+               except AttributeError:
+                   self.dataset['time'] = self.dataset.indexes['time'].to_datetimeindex().normalize()
+               _dataset=self.sel_winters()
+           return _dataset
         elif season == 'all':
            pass # self.dataset = self.dataset
         else:
@@ -59,10 +64,7 @@ class SingleModelPostProcessor(object):
                 compute()
         demeaned = data.groupby('time.dayofyear') - xr_mean
         return demeaned.compute()
-
-    def stats_calc(self,data=None):
-        if data==None:
-            data=self.dataset
+    def stats_calc(self,data):
         mean = data.mean(dim='time')#.rename({'t_prime':'tp_mean'})
         std = data.std(dim='time')#.rename({'t_prime':'tp_std'})
         skewness = xr.full_like(mean,
@@ -73,14 +75,14 @@ class SingleModelPostProcessor(object):
 
     def diagnostic_stats(self,demean=False):
         if 'era5' in self.path_to_files:
-            time_slice_future = slice("2008","2018")
-            time_slice_present = slice("1980","1990")
+            future = (2008,2018)
+            present = (1980,1990)
         else:
-            time_slice_future = slice(2089,2099)
-            time_slice_present = slice(2015,2025)
-        self.data_present = self.dataset.sel(time=time_slice_present)#self.sel_winters(2015,2025)
-        self.data_future = self.dataset.sel(time=time_slice_future)#self.sel_winters(2089,2099)
-    
+            future = (2090,2100)
+            present = (2015,2025)
+        self.data_present = self.sel_winters(present[0],present[1])
+        self.data_future = self.sel_winters(future[0],future[1])
+ 
         if demean:
            try:
                data_present_dm = self.data_present_dm 
@@ -96,11 +98,11 @@ class SingleModelPostProcessor(object):
            self.stats_present = self.stats_calc(self.data_present)
            self.stats_future = self.stats_calc(self.data_future)
            self.stats_diff = self.stats_future - self.stats_present
-    
 
     def diagnostic_plot(self, demean=False):
          self.diagnostic_stats(demean=demean)
          xr_all = xr.concat([self.stats_present,self.stats_future,self.stats_diff],dim='period').assign_coords({'period':['present','future','diff']})
+         print('plotting...')
          p = xr_all.sel(stat='mean')[self.var].plot(transform = ccrs.PlateCarree(),
                                     col='period',
                                     subplot_kws={'projection': ccrs.Orthographic(20, 90)})
