9967abc9c2cf0552979c52c3cdd04da8eede09cb
Amanda Farah <amandamfarah@gmail.com>
2020-12-23 12:25:19 -0600

    debug times for different climate models and add script to run all climate models

---
+++ run_post_proc.py
@@ -0,0 +1,22 @@
+from jetstream.post_proc import SingleModelPostProcessor
+import os
+
+var_of_interest = 'eff_lat'
+path_data = '/project2/moyer/jetsream/era5_processed_data/'
+path_postproc = '/home/afarah/public_html/jetstream/' #'/project2/geos39650/jet_stream/data/post_processing_output/'
+path_aggregate_postproc = '/project2/moyer/jetsream/postproc_aggregate'
+
+for model in os.listdir(path_data):
+#	if model in ['MRI-ESM2-0_hist_tas_daily','GFDL-ESM4_ssp585_tas_daily','CESM2-WACCM_ssp585_tas_daily','UKESM1-0-LL_ssp585_tas_daily','MPI-ESM1-2-HR_ssp585_tas_daily']: 
+#		continue
+	path_processed = f'{path_data}{model}/{model}_{var_of_interest}*.nc4'
+	shortname = model.split('-')[0]
+	print('creating class for ', shortname )
+	single = SingleModelPostProcessor(path_to_files=path_processed,
+                 path_to_save_files=path_postproc+shortname,
+                 diagnostic_var=var_of_interest,
+                 season='DJF')
+
+	single.diagnostic_plot(demean=False)
+
+
